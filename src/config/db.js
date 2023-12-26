0d9ad3441e188725289cdb5316af2c1e7e3d2020
Mehul Dalal <139098439+MehulD96@users.noreply.github.com>
2023-12-26 12:49:04 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export const dbConnection = () => {
-    mongoose.connect('mongodb+srv://Admin:IbOJvqtM0bEkK5GQ@cluster0.9dlts1f.mongodb.net/Blog?retryWrites=true&w=majority'
+    mongoose.connect('database url'
     ).then(() => console.log("connected to database.."))
     .catch((err) =>console.log(err));
 };
